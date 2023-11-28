466e3750bc253954fa351162f79b9732cc5cc31c
miura-code <smiuymiu0official0@gmail.com>
2023-11-28 16:37:14 +0900

    [fix] data.item()に変更
    あとLogの表示を変更

--- train_search.py
+++ train_search.py
@@ -171,7 +171,7 @@ def train(train_queue, valid_queue, model, architect, criterion, optimizer, lr,e
     top5.update(prec5.data.item(), n)
 
     if step % args.report_freq == 0:
-      logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+      logging.info('train %03dsteps %eobjs.avg %ftop1.avg %ftop5.avg', step, objs.avg, top1.avg, top5.avg)
 
   return top1.avg, objs.avg
 
@@ -192,9 +192,9 @@ def infer(valid_queue, model, criterion):
 
     prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
     n = input.size(0)
-    objs.update(loss.data[0], n)
-    top1.update(prec1.data[0], n)
-    top5.update(prec5.data[0], n)
+    objs.update(loss.data.item(), n)
+    top1.update(prec1.data.item(), n)
+    top5.update(prec5.data.item(), n)
 
     if step % args.report_freq == 0:
       logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
