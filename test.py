736da0d5538e182ab6cb33488e45c991b0d21f33
miura-Miura <smiuymiu0official0@gmail.com>
2023-12-31 00:47:21 +0900

    [update] リファクタリング
    arguments関数を追加
    log設定をutils内関数に委託
    保存済みモデルのロードを修正
    Cifar100を利用できるように修正

--- test.py
+++ test.py
@@ -15,45 +15,64 @@ import torch.backends.cudnn as cudnn
 from torch.autograd import Variable
 from model import NetworkCIFAR as Network
 
+def arguments():
+  parser = argparse.ArgumentParser("cifar")
+  parser.add_argument('--data', type=str, default='/home/miura/lab/data', help='location of the data corpus')
+  parser.add_argument('--set', type=str, default='cifar10', help='location of the data corpus')
+  parser.add_argument('--batch_size', type=int, default=96, help='batch size')
+  parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
+  parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
+  parser.add_argument('--init_channels', type=int, default=36, help='num of init channels')
+  parser.add_argument('--layers', type=int, default=20, help='total number of layers')
+  parser.add_argument('--model_path', type=str, default='EXP/model.pt', help='path of pretrained model')
+  parser.add_argument('--auxiliary', action='store_true', default=False, help='use auxiliary tower')
+  parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
+  parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
+  parser.add_argument('--drop_path_prob', type=float, default=0.2, help='drop path probability')
+  parser.add_argument('--save', type=str, default='EXP', help='experiment name')
+  parser.add_argument('--seed', type=int, default=0, help='random seed')
+  parser.add_argument('--arch', type=str, default='DARTS', help='which architecture to use')
+  parser.add_argument('--arch_path', type=str, default=None, help='which saved genotype to use')
 
-parser = argparse.ArgumentParser("cifar")
-parser.add_argument('--data', type=str, default='/home/miura/lab/data', help='location of the data corpus')
-parser.add_argument('--batch_size', type=int, default=96, help='batch size')
-parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
-parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
-parser.add_argument('--init_channels', type=int, default=36, help='num of init channels')
-parser.add_argument('--layers', type=int, default=20, help='total number of layers')
-parser.add_argument('--model_path', type=str, default='EXP/model.pt', help='path of pretrained model')
-parser.add_argument('--auxiliary', action='store_true', default=False, help='use auxiliary tower')
-parser.add_argument('--cutout', action='store_true', default=False, help='use cutout')
-parser.add_argument('--cutout_length', type=int, default=16, help='cutout length')
-parser.add_argument('--drop_path_prob', type=float, default=0.2, help='drop path probability')
-parser.add_argument('--seed', type=int, default=0, help='random seed')
-parser.add_argument('--arch', type=str, default='DARTS', help='which architecture to use')
-args = parser.parse_args()
+  args = parser.parse_args()
+  
+  args.save = '{}'.format(args.arch_path)
 
-log_format = '%(asctime)s %(message)s'
-logging.basicConfig(stream=sys.stdout, level=logging.INFO,
-    format=log_format, datefmt='%m/%d %I:%M:%S %p')
+  print("{0:-<15} args {0:-<15}\n".format(""))
+  for arg in vars(args):
+      print("{:>15}->{}".format(arg, getattr(args, arg)))
 
-CIFAR_CLASSES = 10
-
+  return args
 
 def main():
+  args = arguments()
+
+  CIFAR_CLASSES = 10
+  if args.set=='cifar100':
+    CIFAR_CLASSES = 100
+    
+  log_format = '%(asctime)s %(message)s'
+  logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+      format=log_format, datefmt='%m/%d %I:%M:%S %p')
+  fh = logging.FileHandler(os.path.join(args.save, 'log_test.txt'))
+  fh.setFormatter(logging.Formatter(log_format))
+  logging.getLogger().addHandler(fh)
+  
   if not torch.cuda.is_available():
     logging.info('no gpu device available')
     sys.exit(1)
-
-  np.random.seed(args.seed)
-  torch.cuda.set_device(args.gpu)
-  cudnn.benchmark = True
-  torch.manual_seed(args.seed)
-  cudnn.enabled=True
-  torch.cuda.manual_seed(args.seed)
+    
+  utils.set_seed_gpu(args.seed, args.gpu)
   logging.info('gpu device = %d' % args.gpu)
   logging.info("args = %s", args)
 
-  genotype = eval("genotypes.%s" % args.arch)
+  # genotype = eval("genotypes.%s" % args.arch)
+  genotype = genotypes.load_genotype(args.arch, args.arch_path)
+  if genotype == None:
+    logging.info('no such architecture genotype')
+    sys.exit(1)
+  logging.info('genotype = %s', genotype)
+  
   model = Network(args.init_channels, CIFAR_CLASSES, args.layers, args.auxiliary, genotype)
   model = model.cuda()
   utils.load(model, args.model_path)
@@ -64,17 +83,21 @@ def main():
   criterion = criterion.cuda()
 
   _, test_transform = utils._data_transforms_cifar10(args)
-  test_data = dset.CIFAR10(root=args.data, train=False, download=True, transform=test_transform)
+  
+  if args.set=='cifar100':
+      test_data = dset.CIFAR100(root=args.data, train=False, download=True, transform=test_transform)
+  else:
+      test_data = dset.CIFAR10(root=args.data, train=False, download=True, transform=test_transform)
 
   test_queue = torch.utils.data.DataLoader(
       test_data, batch_size=args.batch_size, shuffle=False, pin_memory=True, num_workers=2)
-
+  
   model.drop_path_prob = args.drop_path_prob
-  test_acc, test_obj = infer(test_queue, model, criterion)
+  test_acc, test_obj = infer(args, test_queue, model, criterion)
   logging.info('test_acc %f', test_acc)
 
 
-def infer(test_queue, model, criterion):
+def infer(args, test_queue, model, criterion):
   objs = utils.AvgrageMeter()
   top1 = utils.AvgrageMeter()
   top5 = utils.AvgrageMeter()
